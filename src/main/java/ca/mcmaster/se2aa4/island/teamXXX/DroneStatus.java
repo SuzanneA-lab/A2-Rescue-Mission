1d31b6b0a253e82df8fa6e6ea8356ccae4cf0811
Suzanne <abduls61@mcmaster.ca>
2025-03-22 22:57:00 -0400

    added further functionality + comments to DroneStatus and MakeDecision classes

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/DroneStatus.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/DroneStatus.java
@@ -7,21 +7,39 @@ import eu.ace_design.island.bot.IExplorerRaid;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+//DroneStatus class manages battery and range status of drone
 public class DroneStatus {
-    private int battery = 100;
+    private int battery;
     private String status = ""; 
-    private String direction = "";
+    //private String direction = ""; // maybe better to remove ?
 
+    //constructor that takes in info JSONOBJECT at initialization stage to get current battery
+    public DroneStatus(JSONOBJECT info){
+        battery = info.getInt("budget");
+    }
+
+    //takes in cost of previous move and deducts it from current battery
     private void UpdateBattery(int cost){
         battery = battery - cost;
     }
 
-    private int getBattery(){
+    //maybe not neccesary? could be completely replaced with getStatus method
+    private int getBattery(){ 
         b = battery;
         return b;
     }
 
-    private String getDirection(){
+    //getStatus method checks both the battery and range status of drone through the response JSONOBJECT
+    //returns true if drone is still fine to proceed, and false if it is either out of battery or out of range
+    private Boolean getStatus(JSONOBJECT response){
+        status = response.getString("status");
+        
+        if (battery > 0 && status.equals("OK")){
+            return true;
+        }
 
+        else {
+            return false;
+        }
     }
 }
--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/MakeDecision.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/MakeDecision.java
@@ -7,11 +7,41 @@ import eu.ace_design.island.bot.IExplorerRaid;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
-// Explorer class based on IExplorerRaid interface
-public class makeDecision{
-    private JSONOBJECT converter = new JSONOBJECT(new JSONTokener(new StringReader(s)));
+// MakeDecision class responsible for managing direction of drone and deciding next move based on radar input
+public class MakeDecision{
+    //private JSONOBJECT converter = new JSONOBJECT(new JSONTokener(new StringReader(s)));
+    private String direction;
+
+    //constructor that takes in info JSONOBJECT in initializatino stage to get direction
+    public MakeDecision(JSONObject info){
+        direction = info.getString("heading");
+    }
     
-    private decideNextMove(JSONOBJECT radarData){
-        String radar = converter.getString("");
+    //decideNextMove method responsible that takes in radarData and returns either instructions to "fly" or the direction the drone should turn in
+    private String decideNextMove(JSONOBJECT radarData){
+        String radar = converter.getString("found");
+        if (radar == "GROUND"){
+            return "fly";
+        }
+
+        else { //drone turns to its left if no ground is located in front of it
+            if (direction.equals("N")){
+                direction = "W";
+            }
+
+            else if (direction.equals("E")){
+                direction = "N";   
+            }
+
+            else if (direction.equals("S")){
+                direction = "E"
+            }
+
+            else if (direction.equals("W")){
+                direction = "S";
+            }
+
+            return direction;
+        }
     }
 }
